f8edfd07da493f002f55a00e4c8edb038b5ee62f
ilSavo <marco.savoia@gmail.com>
2013-12-14 17:51:17 +0100

    Elenco delle nazioni

--- app/scripts/controllers/nationsList.js
+++ app/scripts/controllers/nationsList.js
@@ -1,10 +1,12 @@
 'use strict';
 
-angular.module('compare2AppApp')
-  .controller('NationslistCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+angular.module('compare2App')
+  .controller('NationslistCtrl', ['$scope', '$http', function (scope, http) {
+    http.get('/data/nations.json')
+      .success( function(data) {
+        scope.nations = data;
+      })
+      .error( function() {
+        console.log("Error");
+      });
+  }]);
