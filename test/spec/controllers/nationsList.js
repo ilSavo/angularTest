af3f2d01499802ea374d057089f5508a88789dc5
ilSavo <marco.savoia@gmail.com>
2013-12-14 18:26:38 +0100

    Test basilare funzionante

--- test/spec/controllers/nationsList.js
+++ test/spec/controllers/nationsList.js
@@ -3,20 +3,44 @@
 describe('Controller: NationslistCtrl', function () {
 
   // load the controller's module
-  beforeEach(module('compare2AppApp'));
+  beforeEach(module('compare2App'));
 
   var NationslistCtrl,
-    scope;
+    scope,
+    httpBackend,
+    http;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope, $http, $httpBackend) {
     scope = $rootScope.$new();
+    httpBackend = $httpBackend;
+    http = $http;
+
+    httpBackend
+      .when('GET', '/data/nations.json', {})
+      .respond(function(method, url, data, headers) {
+        return [
+          200,
+          [
+            {'name': 'nation1', 'area': '100', 'population': '500', 'capital': 'Capital 1'},
+            {'name': 'nation2', 'area': '200', 'population': '1000', 'capital': 'Capital 2'},
+            {'name': 'nation3', 'area': '300', 'population': '1500', 'capital': 'Capital 3'},
+          ],
+          {}
+        ];
+      });
+
     NationslistCtrl = $controller('NationslistCtrl', {
-      $scope: scope
+      $scope: scope,
+      $http: http
     });
+
+    httpBackend.flush();
+
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('must return nations', function () {
+    expect(scope.nations).toBeDefined();
   });
+
 });
