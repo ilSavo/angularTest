346c8845977410d2de8cbf81ccd8e42f57cee8eb
ilSavo <marco.savoia@gmail.com>
2013-12-14 19:04:57 +0100

    Aggiornato il controller per soddisfare i test

--- app/scripts/controllers/nationsList.js
+++ app/scripts/controllers/nationsList.js
@@ -1,10 +1,23 @@
 'use strict';
 
 angular.module('compare2App')
-  .controller('NationslistCtrl', ['$scope', '$http', function (scope, http) {
-    http.get('/data/nations.json')
+  .controller('NationslistCtrl', ['$scope', '$http', function ($scope, $http) {
+    $scope.nations = [];
+    $scope.compare = [];
+
+    $http.get('/data/nations.json')
       .success( function(data) {
-        scope.nations = data;
+        $scope.status = true;
+        $scope.nations = data;
+
+        angular.forEach($scope.nations, function (value, key) {
+          //console.log($scope.nations[key].population + " / " + $scope.nations[key].area);
+          $scope.nations[key].area = parseInt($scope.nations[key].area);
+          $scope.nations[key].population = parseInt($scope.nations[key].population);
+          $scope.nations[key].density = value.population / value.area;
+          $scope.nations[key].selected = false;
+          //console.log($scope.nations[key].population / $scope.nations[key].area);
+        });
       })
       .error( function() {
         console.log("Error");
